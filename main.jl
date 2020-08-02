e831755d8046ca430f231d90852b641eaf97042e
marcinnako <43958918+marcinnako@users.noreply.github.com>
2020-08-02 18:33:31 +0200

    Added example anwser

--- main.jl
+++ main.jl
@@ -9,9 +9,9 @@ CUDA.allowscalar(false)
 #Data taken from:
 #https://www.kaggle.com/chetankv/dogs-cats-images
 
-n_train, n_test = 4000, 1000 # Max 4000/1000
-k = 32                # 4000 : 40
-n_epochs = 20
+n_train, n_test = 4000, 1000 # Max 4000/1000 Number of pictures used for training
+k = 25                # Min 32 Batch size used for training.
+n_epochs = 20 #Number of epochs used in training
 rng = Random.shuffle(collect(1:2*n_train))
 X_train = Float32.(zeros(128,128,3,n_train*2))
 X_test = Float32.(zeros(128,128,3,n_test*2))
@@ -31,7 +31,6 @@ for i = 1:n_train #Up to 4000
     X_train[:,:,3,rng[(i*2)]] = Images.blue.(img)
     Y_train[rng[2*i]] = 1
 end
-#X_train = X_train[:,:,:,shuflle(1:end)] #
 for i = 4001:(4000 + n_test) #4000-5000
 
     img = Images.load("dogs_vs_cats\\test_set\\cats\\cat.$i.jpg")
@@ -51,14 +50,11 @@ println("Data Loaded")
 Y_train = onehotbatch(Y_train, 0:1)
 Y_test = onehotbatch(Y_test, 0:1)
 
-
-bs = Int.(floor(n_train/k))
-
+bs = Int.(floor(n_train/k)) # Batching data
 X = [X_train[:,:,:,((i-1)*bs+1):(i*bs)] for i=1:k]
 Y = [Y_train[:,((i-1)*bs+1):(i*bs)] for i=1:k]
 
-
-
+# Creating Neural Network
 m = Chain(
     Conv((3,3), 3=>12 ,pad = (1,1),relu),
     MaxPool((2,2)),
@@ -85,13 +81,9 @@ println("Training Started")
     println("Epoch nr: $i")
     @time for j=1:k
         a, b = X[j] |> gpu , Y[j] |> gpu
-        #b = Y[j] |> gpu
         df = repeated((a,b),1)
-        #cb = Flux.throttle(evalcb,40)
         Flux.train!(loss, params(m), df, opt)
-        a, b = nothing, nothing
-        dataset = nothing
-        GC.gc()
+        a, b, dataset = nothing, nothing, nothing
     end
 end
 println("Training ended")
@@ -99,8 +91,7 @@ m = m |> cpu
 println("Training accuracy: ",accuracy(X[1],Y[1]))
 println("Testing accuracy: ",accuracy(X_test,Y_test))
 
-
-
+#Helping Function used to show pictures and their labels
 function img_show_train(n)
     a = X_train[:,:,:,n]
     a = permutedims(a,[1,2,3])
